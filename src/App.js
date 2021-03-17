5a7026f37b98957261a2b05898c0e70a1f5ec73d
Bryce Speirs <baspeirs@gmail.com>
2021-03-17 19:54:36 -0400

    create react app and first component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>
+                Stevie Boi Welcomes You
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
