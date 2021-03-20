dc8187ac7467388dfe6e336aa2cc9117b3a59ca3
Bryce Speirs <baspeirs@gmail.com>
2021-03-20 13:52:09 -0400

    adding pictures and bootstrap cards to about section and testing design styles

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Container from './Container';
 import Row from './Row';
 import Column from './Column';
+import BassSlappin from '../assets/theFuckHeSmileAt.jpg';
+import MustacheWeildin from '../assets/BerBerDingFlavoredConfustion.jpg';
+import JjsGunTotin from '../assets/jjsWithAGun.jpg'
 
 function Home() {
     return (
@@ -11,17 +14,27 @@ function Home() {
                     <Column size="12">
                         <div className="aboutBox">
                             <Row>
-                                <Column size="6"> 
-                                    <div class="card bg-dark text-white">
-                                        <img src="" id="stevieBoiHero" alt="Stevie Boi slap da bass" />
+                                <Column size="4">
+                                    <div class="card bg-dark text-white aboutImg">
+                                        <img src={BassSlappin} id="bassSlappin" alt="Stevie Boi slap da bass" />
+                                        <div class="card-img-overlay">
+                                            <h3 class="card-title">He Slap Bass</h3>
+                                        </div>
                                     </div>
                                 </Column>
-                                <Column size="6">
-                                    <h4>Bass Slappin'</h4>
+                                <Column size="4">
+                                    <div class="card bg-dark text-white">
+                                        <img src={MustacheWeildin} id="mustacheWeildin" alt="Stevie Boi weild mustache" />
+                                    </div>
+                                </Column>
+                                <Column size="4">
+                                    <div class="card bg-dark text-white">
+                                        <img src={JjsGunTotin} id="jjsGunTotin" alt="Stevie Boi at jjs" />
+                                    </div>
                                 </Column>
                             </Row>
                             <Row>
-                            <Column size="6"> 
+                                <Column size="6">
                                     <div class="card bg-dark text-white">
                                         <img src="" id="stevieBoiHero" alt="Stevie Boi slap da bass" />
                                     </div>
@@ -31,7 +44,7 @@ function Home() {
                                 </Column>
                             </Row>
                             <Row>
-                            <Column size="6"> 
+                                <Column size="6">
                                     <div class="card bg-dark text-white">
                                         <img src="" id="stevieBoiHero" alt="Stevie Boi slap da bass" />
                                     </div>
